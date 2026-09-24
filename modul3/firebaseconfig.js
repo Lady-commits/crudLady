7649b32d9c0a8d29f74adcc30620e4d650ac7d69
Lady-commits <nataliledi12@gmail.com>
2026-09-24 17:46:40 +0700

    Add files via upload

---
+++ modul3/firebaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDNkdITzgANNCaI6fXGbzmmaguRFCwPtZI",
+  authDomain: "crudlady.firebaseapp.com",
+  projectId: "crudlady",
+  storageBucket: "crudlady.firebasestorage.app",
+  messagingSenderId: "469798238238",
+  appId: "1:469798238238:web:6513aec040b6675744d60f",
+  measurementId: "G-8EYGKW50QS"
+};
